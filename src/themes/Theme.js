f085c71ecfc452f4c34d97a60157a7432f643d56
Michael Desanker <mdesanker@gmail.com>
2021-12-11 14:31:31 -0500

    Setup files for project
    commit f8aca5c297a8859413440b083ef8c4c0e4907c6d
    Author: Michael Desanker <mdesanker@gmail.com>
    Date:   Sat Dec 11 14:28:58 2021 -0500
    
        Add theme provider
    
    commit e700e7c1afa1053bcdc79c7ef3f4d80bbc767c77
    Author: Michael Desanker <mdesanker@gmail.com>
    Date:   Sat Dec 11 14:10:49 2021 -0500
    
        Add GlobalStyles
    
    commit a198652bed10bb81bc454ab286a93f5c957916da
    Author: Michael Desanker <mdesanker@gmail.com>
    Date:   Sat Dec 11 13:32:56 2021 -0500
    
        Remove extraneous files
    
    commit 6734e3ef275e3d46d99a8031a8b357e5020d7371
    Author: Michael Desanker <mdesanker@gmail.com>
    Date:   Sat Dec 11 13:31:27 2021 -0500
    
        Write readme
    
    commit ce123bce5117cfa5e6dc2fc0a609c2d9dfb1bf0e
    Author: Michael Desanker <mdesanker@gmail.com>
    Date:   Sat Dec 11 13:28:20 2021 -0500
    
        Set up index.html

---
+++ src/themes/Theme.js
@@ -0,0 +1,12 @@
+const Theme = {
+  colors: {
+    primary: "#1877f2",
+    primaryHover: "#1362ca",
+    secondary: "#42b72a",
+    secondaryHover: "#338e20",
+    lightBackground: "#f0f2f5",
+    mediumBackground: "#e4e6eb",
+  },
+};
+
+export default Theme;
